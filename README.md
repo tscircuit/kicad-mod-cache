4af92d2b9b0032c25ad6badcaf92be43ea8c03b0
seveibar <seveibar@gmail.com>
2024-06-04 15:04:45 -0700

    add cors and kicad_mod filename check

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # @tscircuit/kicad-mod-server
 
 This server serves the kicad mod files using the git repository
+
+| Endpoint            | Description             |
+| ------------------- | ----------------------- |
+| `/kicad_files.json` | List of all kicad files |
+| `/*.kicad_mod`      | Get a kicad mod file    |
--- app/[...anypath]/route.ts
+++ app/[...anypath]/route.ts
@@ -6,6 +6,10 @@ export const GET = async (req) => {
     return new Response("Missing path parameter", { status: 400 })
   }
 
+  if (!pathname.endsWith("kicad_mod")) {
+    return new Response("Invalid path parameter", { status: 400 })
+  }
+
   // Construct the GitLab URL using the extracted parameter
   const gitlabUrl = `https://gitlab.com/kicad/libraries/kicad-footprints/-/raw/master${pathname}?ref_type=heads`
 
@@ -20,9 +24,33 @@ export const GET = async (req) => {
   const headers = new Headers(res.headers)
   headers.set("Cache-Control", "s-maxage=86400, stale-while-revalidate=86400")
 
-  // Return the fetched response with caching headers
+  // Add CORS headers
+  headers.set(
+    "Access-Control-Allow-Origin",
+    "https://github.io, https://tscircuit.com, http://localhost:3000, http://localhost:5173"
+  )
+  headers.set("Access-Control-Allow-Methods", "GET")
+  headers.set("Access-Control-Allow-Headers", "Content-Type")
+
+  // Return the fetched response with caching and CORS headers
   return new Response(await res.text(), {
     status: res.status,
+    headers: headers,
+  })
+}
+
+export const OPTIONS = async (req) => {
+  // Add CORS headers
+  const headers = new Headers()
+  headers.set(
+    "Access-Control-Allow-Origin",
+    "https://github.io, https://tscircuit.com, http://localhost:3000, http://localhost:5173"
+  )
+  headers.set("Access-Control-Allow-Methods", "GET")
+  headers.set("Access-Control-Allow-Headers", "Content-Type")
+
+  return new Response(null, {
+    status: 200,
     headers: headers,
   })
 }
