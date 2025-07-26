371ddc7a93c3d000301766468e6c0a67bacab090
seveibar <seveibar@gmail.com>
2025-07-26 13:56:51 -0700

    improve index, add new details about circuit json variant of api

--- app/route.ts
+++ app/route.ts
@@ -1,7 +1,14 @@
 export const GET = (req) => {
-  return new Response(`<html><body>
+	return new Response(`<html><body>
   
   This server caches official kicad mod files, here is the <a href="https://gitlab.com/kicad/libraries/kicad-footprints/-/blob/master/LICENSE.md?ref_type=heads">license file</a> and <a href="https://gitlab.com/kicad/libraries/kicad-footprints">original repo</a>
+    <p>
+    Example URL: <a href="/kicad-footprints/Resistor_SMD.pretty/R_0402_1005Metric.kicad_mod">/kicad-footprints/Resistor_SMD.pretty/R_0402_1005Metric.kicad_mod</a>
+    </p>
+    <p>
+    You can also load the circuit json equivalent of the mod file by replacing the .kicad_mod with .circuit.json
+    <a href="/kicad-footprints/Resistor_SMD.pretty/R_0402_1005Metric.circuit.json">/kicad-footprints/Resistor_SMD.pretty/R_0402_1005Metric.circuit.json</a>
+    </p>
   
-  </body></html>`)
-}
+  </body></html>`);
+};
